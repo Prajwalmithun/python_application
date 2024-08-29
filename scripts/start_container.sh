19313323356b3a7a721be2eab7651a87d2ae9d68
vanquisher <32311274+Prajwalmithun@users.noreply.github.com>
2024-08-28 20:37:03 -0600

    added appspec

---
+++ scripts/start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit the script if any statement returns a non-true return value
+set -e
+
+# Pull the image from Docker Hub
+docker pull prajwal3498/python_application:v1
+
+# Run the container
+docker run --name python_app_container_01 -dit -p 5001:5001 prajwal3498/python_application:v1
---
+++ scripts/stop_container.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+# Stop the container
+docker stop python_app_container_01
