0a9004dba00aeb8c012e9f1d21b27bb15d17d71e
vanquisher <32311274+Prajwalmithun@users.noreply.github.com>
2024-08-28 22:31:31 -0600

    improved functionality of stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-# Stop the container
-docker stop `docker ps -q`
+# Check if there are any running containers
+if [ "$(docker ps -q)" ]; then
+  echo "Stopping running containers..."
+  
+  # Stop the running containers
+  docker stop $(docker ps -q)
 
-# Remove old containers
-docker rm `docker ps -a -q`
+  # Remove old containers
+  echo "Removing stopped containers..."
+  docker rm $(docker ps -a -q)
+else
+  echo "No running containers found."
+fi
